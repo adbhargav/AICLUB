b6d386c9228c497a765809b3f423fc46a77e0b94
BHARGAV A <bhargavandhe202@gmail.com>
2025-10-10 08:50:58 +0530

    Update Project.js

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 
-const projectSchema = new mongoose.Schema({
-  student: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  description: { type: String },
-  githubLink: { type: String },
-  submissionDate: { type: Date, default: Date.now },
-}, { timestamps: true });
+const projectSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // connects to your User model
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: String,
+    githubLink: String,
+    submissionDate: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Project", projectSchema);
